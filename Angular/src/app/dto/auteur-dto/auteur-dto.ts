60ae9ddeed0fe14b62e07750a8e677f9ef976484
MaxRK <max.rekar@gmail.com>
2025-11-14 17:18:11 +0100

    auteur pour structure

---
+++ Angular/src/app/dto/auteur-dto/auteur-dto.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auteur-dto',
+  imports: [],
+  templateUrl: './auteur-dto.html',
+  styleUrl: './auteur-dto.css',
+})
+export class AuteurDto {
+
+}
