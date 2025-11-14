978463a6403f36cd2c1709a1b4a73503f12aff89
swan <swanmhallais@gmail.com>
2025-11-14 15:42:38 +0100

    hot fix
    app

---
+++ src/main/java/com/example/ProjetEntrainementBibliothequeApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEntrainementBibliothequeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjetEntrainementBibliothequeApplication.class, args);
+  }
+
+}
+
