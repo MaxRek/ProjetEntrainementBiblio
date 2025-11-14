abd977e45ecd66e1c311c3b4e3a6b6f8fe3fef53
MaxRK <max.rekar@gmail.com>
2025-11-14 12:34:00 +0100

    squelette

---
+++ src/test/java/com/example/projetEntrainementSopra/ProjetEntrainementSopraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetEntrainementBibiliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEntrainementBibiliothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
