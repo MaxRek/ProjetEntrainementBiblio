8a6a00c1b64e38a04a41921df9933d54f4df1885
MaxRK <max.rekar@gmail.com>
2025-11-14 14:00:29 +0100

    correction renommage

--- src/test/java/com/example/ProjetEntrainementBibliotheque/ProjetEntrainementBibiliothequeApplicationTests.java
+++ src/test/java/com/example/ProjetEntrainementBibliotheque/ProjetEntrainementBibiliothequeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.ProjetEntrainementBibiliotheque;
+package com.example.ProjetEntrainementBibliotheque;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
