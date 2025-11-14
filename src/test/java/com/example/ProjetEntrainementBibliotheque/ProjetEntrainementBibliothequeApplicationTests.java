531dfa807a6209fc9793664e102883b68c6b8a7d
MaxRK <max.rekar@gmail.com>
2025-11-14 13:00:00 +0100

    correction

---
+++ src/test/java/com/example/ProjetEntrainementBibliotheque/ProjetEntrainementBibliothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetEntrainementBibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEntrainementBibliothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
