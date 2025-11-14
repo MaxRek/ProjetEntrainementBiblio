7cde77828fe708a471ee235bc1cad4dfa6a1de11
MaxRK <max.rekar@gmail.com>
2025-11-14 14:52:21 +0100

    ajout classe auteur

--- src/main/java/com/example/model/Auteur.java
+++ src/main/java/com/example/model/Auteur.java
@@ -2,6 +2,7 @@ package com.example.model;
 
 import org.hibernate.annotations.ValueGenerationType;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,8 +13,61 @@ public class Auteur {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Column
 	private String nom;
+	
+	@Column
 	private String prenom;
+	
+	@Column
 	private String nationalite;
+
+	public Auteur() {}
+
+	public Auteur(String nom, String prenom, String nationalite) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.nationalite = nationalite;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNationalite() {
+		return nationalite;
+	}
+
+	public void setNationalite(String nationalite) {
+		this.nationalite = nationalite;
+	}
+
+	@Override
+	public String toString() {
+		return "Auteur [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", nationalite=" + nationalite + "]";
+	}
+	
+	
+	
 	
 }
