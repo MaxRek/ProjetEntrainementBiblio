b1a7ecf9524c6b8b6a649e53573205a453df9763
swan <swanmhallais@gmail.com>
2025-11-14 15:27:56 +0100

    Collection

---
+++ src/main/java/com/example/dao/IDAOCollection.java
@@ -0,0 +1,13 @@
+package com.example.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.model.Collection;
+
+public interface IDAOCollection extends JpaRepository<Collection, Integer>{
+
+	public List<Collection> findByLibelleContaining(String recherche);
+	
+}
