af243b030a516810cd0fe3f30c7f483b8484657d
swan <swanmhallais@gmail.com>
2025-11-17 12:56:18 +0100

    route

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { LivreAll } from './page/livre/livre-all/livre-all';
 import { AuteurAll } from './page/auteur/auteur-all/auteur-all';
 import { Collection } from './page/collection/collection';
-//import { EditeurAll } from './page/editeur/editeur-all/editeur-all';
+import { EditeurAll } from './page/editeur/editeur-all/editeur-all';
 
 export const routes: Routes = [
     { path: 'livre', component: LivreAll },
     { path: 'auteur', component: AuteurAll},
     { path: 'collection', component: Collection},
-    //{ path: 'editeur', component: EditeurAll},
+    { path: 'editeur', component: EditeurAll},
 ];
 
