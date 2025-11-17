8dd4bc64abc122a71ce20e4c5b9987dfb2bff8b1
MaxRK <max.rekar@gmail.com>
2025-11-17 10:53:56 +0100

    Auteur service, page

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AuteurAll } from './page/auteur/auteur-all/auteur-all';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'auteur', component: AuteurAll}
+];
