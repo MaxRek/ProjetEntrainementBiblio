4a8bc272d1d099d0b1e4a00d5c3cca1f08c903c7
Viviana <viviana.srr21@gmail.com>
2025-11-17 11:25:38 +0100

    Auto stash before merge of "main" and "origin/main"
    Modif Navigation et App-Routes pour tous + Livre (dto, service, page)

--- Angular/src/app/dto/livre-dto.ts
+++ Angular/src/app/dto/livre-dto.ts
@@ -1,2 +1,81 @@
 export class LivreDto {
+
+    constructor(
+    private _id:number,
+	private _titre:string,
+    private _resumer:string,
+    private _annee:string,
+    private _auteur: any,
+    private _editeur: any,
+    private _collection?: any
+    ) {}
+	
+public get id(): number {
+    return this._id;
+}
+
+public set id(value: number) {
+    this._id = value;
+}
+
+public get titre(): string {
+    return this._titre;
+}
+
+public set titre(value: string) {
+    this._titre = value;
+}
+
+public get resumer(): string {
+    return this._resumer;
+}
+
+public set resumer(value: string) {
+    this._resumer = value;
+}
+
+public get annee(): string {
+    return this._annee;
+}
+
+public set annee(value: string) {
+    this._annee = value;
+}
+
+    public get auteur(): any {
+        return this._auteur;
+    }
+
+    public set auteur(value: any) {
+        this._auteur = value;
+    }
+
+    public get editeur(): any {
+        return this._editeur;
+    }
+
+    public set editeur(value: any) {
+        this._editeur = value;
+    }
+
+    public get collection(): any | undefined {
+        return this._collection;
+    }
+
+    public set collection(value: any | undefined) {
+        this._collection = value;
+    }
+
+
+    public toJson(): any {
+        return {
+            id: this._id ?? null,
+            titre: this._titre,
+            resumer: this._resumer,
+            annee: this._annee,
+            auteur: this._auteur,
+            editeur: this._editeur,
+            collection: this._collection ?? null
+        };
+    }
 }
--- Angular/src/app/page/livre/livre-all/livre-all.ts
+++ Angular/src/app/page/livre/livre-all/livre-all.ts
@@ -1,11 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LivreDto } from '../../../dto/livre-dto';
+import { FormGroup, FormControl, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LivreService } from '../../../service/livre-service';
 
 @Component({
   selector: 'app-livre-all',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './livre-all.html',
   styleUrl: './livre-all.css',
 })
-export class LivreAll {
+export class LivreAll implements OnInit {
 
+  protected livres$!: Observable<LivreDto[]>;
+
+  protected livreForm!: FormGroup;
+  protected titreCtrl!: FormControl;
+  protected resumerCtrl!: FormControl;
+  protected anneeCtrl!: FormControl;
+  protected auteurCtrl!: FormControl;
+  protected editeurCtrl!: FormControl;
+  protected collectionCtrl!: FormControl;
+
+  protected editingLivre!: LivreDto | null;
+
+  constructor(
+    private livreService: LivreService, 
+    private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.reloadLivres();
+
+    this.titreCtrl = this.formBuilder.control('', Validators.required);
+    this.resumerCtrl = this.formBuilder.control('', [Validators.required, Validators.maxLength(500)]);
+    this.anneeCtrl = this.formBuilder.control('', [Validators.required, Validators.minLength(4), Validators.maxLength(4)]);
+    this.auteurCtrl = this.formBuilder.control(null);
+    this.editeurCtrl = this.formBuilder.control(null);
+    this.collectionCtrl = this.formBuilder.control(null);
+
+    this.livreForm = this.formBuilder.group({
+      titre: this.titreCtrl,
+      resumer: this.resumerCtrl,
+      annee: this.anneeCtrl,
+      auteur: this.auteurCtrl,
+      editeur: this.editeurCtrl,
+      collection: this.collectionCtrl
+    });
+  }
+
+  public trackLivre(index: number, value: LivreDto) {
+    return value.id;
+  }
+
+  private reloadLivres(): void {
+    this.livres$ = this.livreService.findAll();
+  }
+
+  public creerOuModifierLivre(): void {
+    if (this.livreForm.invalid) {
+      this.livreForm.markAllAsTouched();
+      return;
+    }
+
+    const formValue = this.livreForm.value;
+
+    let dto: LivreDto;
+
+    if (this.editingLivre) {
+      // modification
+      dto = new LivreDto(
+        this.editingLivre.id,
+        formValue.titre,
+        formValue.resumer,
+        formValue.annee,
+        formValue.auteur ?? this.editingLivre.auteur,
+        formValue.editeur ?? this.editingLivre.editeur,
+        formValue.collection ?? this.editingLivre.collection
+      );
+    } else {
+      // création
+      dto = new LivreDto(
+        0,
+        formValue.titre,
+        formValue.resumer,
+        formValue.annee,
+        formValue.auteur ?? null,
+        formValue.editeur ?? null,
+        formValue.collection ?? null
+      );
+    }
+
+    this.livreService.save(dto);
+    this.editingLivre = null;
+    this.livreForm.reset();
+    this.reloadLivres(); 
+  }
+
+  public editer(livre: LivreDto) {
+    this.editingLivre = livre;
+
+    this.livreForm.patchValue({
+      titre: livre.titre,
+      resumer: livre.resumer,
+      annee: livre.annee,
+      auteur: livre.auteur,
+      editeur: livre.editeur,
+      collection: livre.collection
+    });
+  }
+
+  public supprimerLivre(livre: LivreDto): void {
+    this.livreService.deleteById(livre.id);
+    this.reloadLivres();
+  }
 }
